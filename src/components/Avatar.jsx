044d6c377f324e5bfc69e8f9f4f876646bcd0e71
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-08-10 02:37:49 +0300

    after lesson

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,25 @@
+import {useDispatch, useSelector} from "react-redux";
+import {changeAvatar, changeName} from "../actions/userAction.js";
+
+const Avatar = ({size}) => {
+    const {avatar, name} = useSelector(state => state.user);
+    const dispatch = useDispatch();
+    return (
+        <img
+            onClick={() => {
+                const url = prompt('Enter new avatar url');
+                dispatch(changeAvatar(url));
+            }}
+            onContextMenu={(e) => {
+                e.preventDefault();
+                const name = prompt('Enter new name');
+                dispatch(changeName(name));
+            }}
+            className={`user-avatar ${size ?? ''}`}
+            src={avatar}
+            alt={name}
+        />
+    );
+};
+
+export default Avatar;
---
+++ src/components/Stats.jsx
@@ -0,0 +1,22 @@
+import Avatar from "./Avatar.jsx";
+import {useSelector} from "react-redux";
+
+const Stats = () => {
+    const {followers, following} = useSelector(state => state.stats);
+    const  {name} = useSelector(state => state.user);
+
+    return (
+        <div className={'user-stats'}>
+            <div>
+                <Avatar/>
+                {name}
+            </div>
+            <div className={'stats'}>
+                <div>Followers: {followers}</div>
+                <div>Following: {following}</div>
+            </div>
+        </div>
+    );
+};
+
+export default Stats;
